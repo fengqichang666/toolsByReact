1efa2408612161ee528bed9b491f593e9ec8cb0a
qichang.feng <qichang.feng@ebestmobile.com>
2023-11-06 18:10:15 +0800

    记事本

--- src/pages/memorandum/index.tsx
+++ src/pages/memorandum/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { Col, Row, Input, Button } from 'antd';
 import { Data } from './index.model'
 function Memorandum() {
@@ -12,10 +12,18 @@ function Memorandum() {
         newData.splice(index, 1)
         setData(newData)
     }
+    const changeItem = (e: ChangeEvent<HTMLInputElement>, i: number) => {
+        data[i].text = e.target.value;
+        let newData = data.slice();
+        setData(newData)
+    }
+    const getList = () => {
+        console.log(data)
+    }
     return (
         <>
             <Row className="mt16">
-                <Col span={1} offset={11}>
+                <Col span={1} offset={12}>
                     <Button onClick={addItem}>添加</Button>
                 </Col>
             </Row>
@@ -26,7 +34,7 @@ function Memorandum() {
                             {item.serialNumber}、
                         </Col>
                         <Col span={16}>
-                            <Input value={item.text}></Input>
+                            <Input value={item.text} onChange={(e) => changeItem(e, index)}></Input>
                         </Col>
                         <Col span={1} className='ml12'>
                             <Button onClick={() => delItem(index)}>删除</Button>
